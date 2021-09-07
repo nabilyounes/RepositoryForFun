5bd419dcda223562545df8492e7d88d388cd9583
Nabil <nabil.younes@outlook.fr>
2021-09-07 11:18:17 +0200

    adding some changes

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
+  float *phase;
+  float radius, pressure;
   printf("I also want to make changes here \n");
   printf("Hello World");
 // Hello World for testing and entertaining 
   printf("Another Hello World");
+  phase = new float[10]
+  for(int i = 0; i < 10 ; i++)
+  {
+      phase[i] = radius*pressure;
+  }
   return 0;
 }
